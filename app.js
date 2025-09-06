1168e4ded02b894d0f026cebcf391d503a6aad7e
sabrisoma <somacalsabrinaa@gmail.com>
2025-09-06 00:27:44 -0300

    ecommers

---
+++ app.js
@@ -0,0 +1,41 @@
+// Lista de Productos// 
+
+const productos = [
+    { id: 1, nombre: "Agenda 2025 Blanca", precio: 21700 },
+    { id: 2, nombre: "Agenda 2025 Ela", precio: 21700 },
+    { id: 3, nombre: "Combo Ela", precio: 16800 },
+    { id: 4, nombre: "Mate Campo Lola", precio: 23600 },
+    { id: 5, nombre: "Vela Horus", precio: 20000 },
+    { id: 6, nombre: "Mate Gardenia", precio: 17000 },
+    { id: 7, nombre: "Mate Emilia", precio: 14000 },
+    { id: 8, nombre: "Combo Tina", precio: 16800 },
+]
+
+// carrito vacio//
+let carrito = []
+
+// funcion para mostrar productos disponibles//
+
+function mostrarProductos() {
+    console.log("Lista de productos disponibles");
+    for (let i = 0; i < productos.length; i++) {
+        console.log(productos[i].id + ":" + productos[i].nombre + "$" + productos[i].precio + "\n");
+    }
+}
+
+function buscarProducto(producto) {
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].id == producto) {
+            return productos[i].nombre
+        }
+    }
+}
+
+// funcion para agregar productos al carrito"
+mostrarProductos();
+let productoSeleccionado = prompt("Elegi un producto para agregar al carrito");
+
+carrito.push(buscarProducto(productoSeleccionado));
+
+
+console.log("Tu carrito contiene " + carrito);
