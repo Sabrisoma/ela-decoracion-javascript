7703155cd2896dcc963ea9de53683eb00f30c86e
sabrisoma <somacalsabrinaa@gmail.com>
2025-10-25 12:56:06 -0300

    Inicio de Proyecto Final

---
+++ notificaciones.js
@@ -0,0 +1,82 @@
+/* import Swal from "sweetalert2";
+
+export function confirmarEliminarProducto() {
+    return Swal.fire({
+        title: "Estas seguro de eliminar el producto?",
+        text: "No podrás volver atrás",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#f07ed3ff",
+        cancelButtonColor: "rgba(250, 181, 246, 1)",
+        confirmButtonText: "Sí, eliminar!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Eliminado",
+                text: "El Producto ha sido Eliminado",
+                icon: "success"
+            });
+        }
+    });
+}
+
+confirmarEliminarProducto() */
+
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm"; //revisar antes de entregar si hay otra forma de resolverlo
+
+export function confirmarAgregarProducto(nombreProducto) {
+    return Swal.fire({
+        title: "¿Agregar al carrito?",
+        text: `¿Desea agregar "${nombreProducto}" al carrito?`,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Sí, agregar",
+        cancelButtonText: "Cancelar",
+        confirmButtonColor: "#f07ed3ff",
+        cancelButtonColor:"#f07ed3ff",
+    });
+}
+
+export function confirmarEliminarProducto(nombreProducto) {
+    return Swal.fire({
+        title: "Estas seguro de eliminar el producto?",
+        text: "No podrás volver atrás",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#f07ed3ff",
+        cancelButtonColor: "rgba(250, 181, 246, 1)",
+        confirmButtonText: "Sí, eliminar!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Eliminado",
+                text: "El Producto ha sido Eliminado",
+                icon: "success"
+            });
+        }
+    });
+}
+
+export function mostrarToastExito(mensaje) {
+    Swal.fire({
+        toast: true,
+        position: "top-end",
+        icon: "success",
+        title: mensaje,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+    })
+}
+
+export function mostrarToastError(mensaje) {
+    Swal.fire({
+        toast: true,
+        position: "top-end",
+        icon: "error",
+        title: mensaje,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+    })
+}
---
+++ productos.js
@@ -0,0 +1,47 @@
+import { manejarAgregarAlCarrito } from "./carrito.js";
+
+async function obtenerListaProductos() {
+    const response = await fetch("https://fakestoreapi.com/products/?limit=3");
+    const data = await response.json();
+
+    return data;
+
+}
+
+
+export async function mostrarCardsProductos() {
+    try {
+        const productoList = await obtenerListaProductos();
+
+        productoList.forEach((producto) => {
+            crearTarjetaProducto(producto);
+
+        });
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+function crearTarjetaProducto(producto) {
+    const div = document.getElementById("output");
+    const tarjeta = document.createElement("div");
+
+    tarjeta.innerHTML = `
+    <img src="${producto.image}" alt="${producto.title}" class ="product-image">
+    <h4>${producto.title}</h4>
+    <p class="product-price">$${producto.price} </p>
+    <button class= "btn-comprar"> Agregar </button>
+    `;
+
+    const botonComprar=tarjeta.querySelector(".btn-comprar");
+
+    botonComprar.addEventListener("click", function(){
+        manejarAgregarAlCarrito(producto.id, producto.title, producto.price);
+    });
+
+    div.appendChild(tarjeta);
+
+}
+
