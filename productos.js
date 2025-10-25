1ba173440abbde5e73caad360b6bb494f58728be
sabrisoma <somacalsabrinaa@gmail.com>
2025-10-25 14:56:35 -0300

    agregado de mis productos

--- productos.js
+++ productos.js
@@ -1,11 +1,7 @@
 import { manejarAgregarAlCarrito } from "./carrito.js";
 
 async function obtenerListaProductos() {
-    const response = await fetch("https://fakestoreapi.com/products/?limit=3");
-    const data = await response.json();
-
-    return data;
-
+    return matesJson;
 }
 
 
@@ -44,4 +40,51 @@ function crearTarjetaProducto(producto) {
     div.appendChild(tarjeta);
 
 }
+
+
+const matesJson = [
+  {
+    "id": 1,
+    "title": "Mate Emilia",
+    "price": "18.500",
+    "description": "descripcion mate 1",
+    "image": "./imagenes/emilia.jpeg"
+  },
+  {
+    "id": 2,
+    "title": "Combo Agnes",
+    "price": "25.200",
+    "description": "description mate 2",
+    "image": "./imagenes/agnes.jpeg"
+  },
+    {
+    "id": 3,
+    "title": "Agenda Ela",
+    "price": "30.000",
+    "description": "description mate 2",
+    "image": "./imagenes/agendaela.png"
+  },
+    {
+    "id": 4,
+    "title": "Agenda Blanca",
+    "price": "30.000",
+    "description": "description mate 2",
+    "image": "./imagenes/agendablanca.png"
+  },
+    {
+    "id": 5,
+    "title": "Combo Ela",
+    "price": "18.500",
+    "description": "description mate 2",
+    "image": "./imagenes/comboela.jpeg"
+  },
+    {
+    "id": 6,
+    "title": "Mate Gardenia",
+    "price": "16.000",
+    "description": "description mate 2",
+    "image": "./imagenes/mategardenia.jpeg"
+  },
+]
+
 
