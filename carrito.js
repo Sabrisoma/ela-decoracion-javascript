7fdeacff2c5370cc54b402fa1e531e0f34e5dc20
sabrisoma <somacalsabrinaa@gmail.com>
2025-10-26 12:48:51 -0300

    calculo total carrito

--- carrito.js
+++ carrito.js
@@ -9,7 +9,7 @@ function agregarAlCarrito(title, precio) {
     const itemCarrito = document.createElement("div");
 
     itemCarrito.innerHTML = `
-    <span class="item-info">
+    <span class="item-info" data-precio="${precio}">
       <strong>${title}</strong> - $${precio}
     </span>
     <button class="btn-eliminar">Eliminar</button>
@@ -22,9 +22,7 @@ function agregarAlCarrito(title, precio) {
 
     divCarrito.appendChild(itemCarrito);
     const totalCarrito = document.getElementById("totalCarrito");
-    totalCarrito.textContent = totalCarrito.textContent + 2;
-
-
+    totalCarrito.textContent = calcularTotalCarrito();
 }
 
 export async function manejarAgregarAlCarrito(id, title, precio) {
@@ -39,6 +37,18 @@ async function manejarEliminarDelCarrito(itemCarrito, title) {
     const resultado = await confirmarEliminarProducto(title);
     if (resultado.isConfirmed) {
         itemCarrito.remove();
+        const totalCarrito = document.getElementById("totalCarrito");
+        totalCarrito.textContent = calcularTotalCarrito();
         mostrarToastExito("Producto eliminado con éxito");
     }
+}
+
+function calcularTotalCarrito() {
+    const items = document.querySelectorAll('.item-info');
+    let total = 0;
+
+    items.forEach(item => {
+        total += Number(item.dataset.precio);
+    });
+    return total;
 }
